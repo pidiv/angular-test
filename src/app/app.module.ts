98a7057ca679d71ed1a1e85ea22439fa0b1a2151
Piotr Dziubek <piotr.dziubek@vimanet.com>
2021-05-21 12:39:05 +0200

    Add HotjarID

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxHotjarModule } from 'ngx-hotjar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxHotjarModule.forRoot("2413281")
   ],
   providers: [],
   bootstrap: [AppComponent]
